f16ac0bc0a8fda08bf59d0cfd2a0498fcb426a9a
Reyas Khan M <119721119+rewyekha@users.noreply.github.com>
2024-03-15 09:20:13 +0530

    init

---
+++ Otp/main.py
@@ -0,0 +1,60 @@
+from multiprocessing.sharedctypes import Value
+import tkinter
+from tkinter import messagebox
+import customtkinter
+
+# Set appearance mode and default color theme for customtkinter
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+
+# Create the main application window
+app = customtkinter.CTk()
+
+# Set screen width and height
+screen_width = 1920
+screen_height = 1080
+
+# Set window geometry
+app.geometry(f"500x450+{(screen_width//2)-300}+{(screen_height//2)-300}")
+app.title("Kamikaze")
+
+# Function for handling login
+def login():
+    if entry_1.get() == "learningpurpose0316@gmail.com" and entry_2.get() == "3434":
+        messagebox.showinfo("Login Successful!")
+    else:
+        messagebox.showinfo("Error!", "Incorrect email or password")
+
+# Create a frame within the application
+frame_1 = customtkinter.CTkFrame(master=app)
+frame_1.pack(pady=30, padx=30, fill="both", expand=True)
+
+# Create and pack labels for email and password
+label_1 = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT, text="E-mail: ")
+label_1.pack(pady=10, padx=10)
+
+# Create and pack an entry widget for email
+entry_1 = customtkinter.CTkEntry(master=frame_1, width=150, placeholder_text="examples@gmail.com")
+entry_1.pack(pady=10, padx=10)
+
+# Create and pack labels for password
+label_2 = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT, text="Password: ")
+label_2.pack(pady=10, padx=10)
+
+# Create and pack an entry widget for password
+entry_2 = customtkinter.CTkEntry(master=frame_1, width=150, placeholder_text="********", show="*")
+entry_2.pack(pady=10, padx=10)
+
+# Create and pack a button for login
+button_1 = customtkinter.CTkButton(master=frame_1, command=login, text="Login")
+button_1.pack(pady=30, padx=10)
+
+# Create and pack a checkbox for email reminder
+checkbox_1 = customtkinter.CTkCheckBox(master=frame_1, text="Remember Email")
+checkbox_1.pack(pady=12, padx=10)
+
+# Set the initial value for radiobutton_var
+radiobutton_var = tkinter.IntVar(value=1)
+
+# Start the main event loop
+app.mainloop()
---
+++ Otp/otp.py
@@ -0,0 +1,56 @@
+import tkinter
+import customtkinter
+import random
+from smtplib import SMTP
+from tkinter import messagebox
+
+def run():
+    # Sending email
+    code = str(random.randint(000000, 1000000))
+    subject = "Your Verification Code: "
+    message = code
+    content = "Subject: {0}\n\n{1}".format(subject, message)
+
+    my_email_address = "...@outlook.com"
+    password = "pass"
+    send_to = "...@outlook.com"
+
+    mail = SMTP("smtp.outlook.com", 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(my_email_address, password)
+    mail.sendmail(my_email_address, send_to, content.encode("utf-8"))
+    messagebox.showinfo("Successful!", "Verification code sent via email")
+
+    # Verification screen
+    customtkinter.set_appearance_mode("white")
+    customtkinter.set_default_color_theme("green")
+    app = customtkinter.CTk("")
+    screen_width = 1920
+    screen_height = 1080
+    app.geometry(f"300x300+{(screen_width//2)}+{(screen_height//2)-200}")
+    app.title("OTP")
+
+    def login():
+        if entry.get() == code:
+            messagebox.showinfo("Successful!", "Welcome")
+            app.destroy()
+        else:
+            messagebox.showinfo("Error!", "Invalid code")
+
+    frame_1 = customtkinter.CTkFrame(master=app)
+    frame_1.pack(pady=20, padx=40, fill="both", expand=True)
+
+    label_1 = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT, text="Enter the code: ")
+    label_1.pack(pady=10, padx=10)
+
+    entry = customtkinter.CTkEntry(master=frame_1, placeholder_text="4CSAQ")
+    entry.pack(pady=10, padx=10)
+
+    button_1 = customtkinter.CTkButton(master=frame_1, command=login, text="Login")
+    button_1.pack(pady=30, padx=10)
+
+    app.mainloop()
+
+# Run the function
+run()
